3a50a90477c3c676c1a11a08892d6a87a8f64259
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-02 23:45:24 +0200

    added admin and models to db

--- news/tests.py
+++ news/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Editor, tags, Article
 
 # Create your tests here.
+class EditorTestClass(TestCase):
+
+    #setup method
+    def setUp(self):
+        self.Missy = Editor(first_name = 'Missy', last_name = 'Olivia', email = 'ngamissia@gmail.com')
+    
+    #Testing Instance
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Missy, Editor))
+
+    #testing save method
+    def test_save_method(self):
+        self.Missy.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors)> 0 )
